b948db88fa49b99388df95fba59792440e253628
devesh sharma <deveshsharma7618@gmail.com>
2025-10-05 04:17:58 +0000

    made the basic template

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def HomePage():
 def LoginPage():
     return render_template("login.html")
 
+@app.route("/create-account")
+def CreateAccount():
+    return render_template("create-account.html")
+
+@app.route("/blogs")
+def BlogsPage():
+    return render_template("blogs.html")
+
+@app.route("/contact-us")
+def ContactUsPage():    
+    return render_template("contact-us.html")
 
 if __name__ == "__main__":
-    app.run(port = 5000)
+    app.run(port = 5001)
